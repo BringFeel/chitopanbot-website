86e4da4f147b5ad0f218a923bc21d3326aebc471
BringFeel <69062690+BringFeel@users.noreply.github.com>
2022-06-29 22:48:39 -0300

    Nueva API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,23 +1,30 @@
-$(document).ready(function(){
-    $.get('http://si.bringfeel.com:27826',function(data){
-        var dJson = JSON.parse(data);
-        $('#servidores').text(dJson.servidores);
-        $('#ramusadaapi').text(dJson.ramusada);
-        $('#RamUsada').text(Math.floor(dJson.ramusada / 1024 / 1000));
-        $('#RamLibre').text(Math.floor(4096 - (dJson.ramusada / 1024 / 1000)));
-        $('#RamTotal').text(dJson.ramtotal);
-        $('#uptime').text(dJson.uptime);
-        $('#cpus').text(Math.floor(dJson.cpus));
-        $('#cpu').text(dJson.cpu);
-	    $('#estado').text(dJson.estado);
-	    $('#estadoapi').text(dJson.eapi);
-		$('#ping').text(dJson.ping);
-		$('#cpuv').text(dJson.cpuv);
-		$('#so').text(dJson.so);
-		$('#arq').text(dJson.arq);
-		$('#djs').text(dJson.djs);
-		$('#njs').text(dJson.njs);
-		$('#smusic').text(dJson.smusic);
-		$('#usuarios').text(dJson.usuarios);
-    });
-});
+function fetchData() {
+	fetch('https://api.statcord.com/v3/779841907484262421').then(response => {
+		return response.json();
+	}).then(data => {
+
+		$('#servidores').text(data.data[0].servers);
+        $('#RamUsada').text(`${Math.floor(data.data[0].memactive / 1048576)}Mb`);
+        $('#RamLibre').text(`${Math.floor(4096 - (data.data[0].memactive / 1048576))}Mb`);
+        $('#RamTotal').text('4096Mb');
+        $('#RedUsada').text(`${Math.floor(data.data[0].bandwidth / 1048576)}Mb`);
+        $('#usuarios').text(data.data[0].users);
+        $('#njs').text('v13.8.1');
+        $('#djs').text('v17.9.1');
+
+        if (data.error = false) {
+        $('#estado').text('Online');
+        }
+        else {
+        $('#estado').text('Offline');
+        }
+
+        if (data.error = true) {
+        $('#estadoapi').text(``);
+        }
+	}).catch(error => {
+		console.log(error);
+	});
+}
+
+fetchData();
